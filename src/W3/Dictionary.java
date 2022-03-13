b1ddc19b5afc221cacc2dcd345b1da5df309d05b
ZJ <zhaozj1225@outlook.com>
2022-03-13 17:54:59 +1100

    13 Mar

--- src/W3/Dictionary.java
+++ src/W3/Dictionary.java
@@ -44,14 +44,14 @@ public class Dictionary {
 
     public static void main(String[] args) {
         // TODO
-        if (args.length > 0) {
-            Dictionary[] results = readDictionary(args[0]);
+        if (args.length == 0) {
+            Dictionary[] results = readDictionary("C:\\Users\\1\\IdeaProjects\\9003\\src\\W3\\sample.dict");
             Scanner scan = new Scanner(System.in);
 
             while (scan.hasNextLine()) {
                 String input = scan.nextLine();
                 if (!input.equals(""))
-                    System.out.println(matchEntry(results, scan.nextLine()));
+                    System.out.println(matchEntry(results, input));
                 else
                     break;
             }
