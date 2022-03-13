de00fb7bc85c803b7ddcade181dfa5cf91cdcd5a
ZJ <zhaozj1225@outlook.com>
2022-03-13 17:50:19 +1100

    13 Mar

---
+++ src/W3/Dictionary.java
@@ -0,0 +1,67 @@
+package W3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Dictionary {
+
+    private String entry;
+    private String definition;
+
+    public Dictionary(String entry, String definition) {
+        this.entry = entry;
+        this.definition = definition;
+    }
+
+    public static Dictionary[] readDictionary(String path) {
+        Dictionary[] entries = new Dictionary[500];
+        int count = 0;
+
+        File f = new File(path);
+        try {
+            Scanner scan = new Scanner(f);
+            while (scan.hasNextLine()) {
+                String s = scan.nextLine();
+                String[] temp = s.split(" \\u003f "); //[?] //\\?
+                entries[count++] = new Dictionary(temp[0], temp[1]);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No such dictionary");
+        }
+
+        return entries;
+    }
+
+    public static String matchEntry(Dictionary[] entries, String entry) {
+        for (Dictionary i: entries)
+            if (i != null)
+                if (i.entry.equals(entry))
+                    return i.definition;
+
+        return "No matching definition";
+    }
+
+    public static void main(String[] args) {
+        // TODO
+        if (args.length > 0) {
+            Dictionary[] results = readDictionary(args[0]);
+            Scanner scan = new Scanner(System.in);
+
+            while (scan.hasNextLine()) {
+                String input = scan.nextLine();
+                if (!input.equals(""))
+                    System.out.println(matchEntry(results, scan.nextLine()));
+                else
+                    break;
+            }
+//            String a = matchEntry(results, scan.nextLine());
+//            System.out.println(a);
+
+        }
+        else
+            System.out.println("No dictionary provided");
+
+    }
+
+}
---
+++ src/W3/toHex.java
@@ -0,0 +1,16 @@
+package W3;
+
+import java.lang.*;
+
+public class toHex {
+    public static void main(String[] args) {
+        int tester = 13595;
+        String S = Integer.toHexString(tester);
+        System.out.println(S);
+        System.out.println(S.toUpperCase());
+
+
+    }
+
+
+}
