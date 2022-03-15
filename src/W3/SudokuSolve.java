f0d24a60235c11810f936142dfb0cb1b30097290
ZJ <zhaozj1225@outlook.com>
2022-03-15 15:49:47 +1100

    15 Mar

--- src/W3/SudokuSolve.java
+++ src/W3/SudokuSolve.java
@@ -4,9 +4,15 @@ import java.util.Arrays;
 
 public class SudokuSolve {
     public static boolean isArrayValid(Integer[] x) {
+
         int[] a = new int[x.length];
-        for(int i=0; i<x.length;i++)
-            a[i] = x[i];
+        for(int i=0; i<x.length;i++) {
+            if (x[i] != null) {
+                a[i] = x[i];
+            }
+
+        }
+
 
         Arrays.sort(a);
         for (int i = 1; i < a.length; i++) {
@@ -19,39 +25,36 @@ public class SudokuSolve {
     public static boolean isSolvable(Integer[][] board) {
         //TODO
 
-        if (board == null) {
-//            System.out.println(1);
+        if (board == null)
             return false;
-        }
 
-        if (board.length % 3 != 0) {
-//            System.out.println(2);
+        if (board.length % 3 != 0)
             return false;
-        }
 
-        for (Integer[] i: board) {
-            if (i == null) {
-//                System.out.println(3);
+        for (int i=0;i<board.length;i++) {
+            if (board[i] == null)
                 return false;
-            }
 
-            if (i.length % 3 != 0) {
-//                System.out.println(4);
+            if (board[i].length % 3 != 0)
                 return false;
-            }
 
-            for (Integer j: i) {
-                if (j == null) {
-//                    System.out.println(5);
-                    return false;
+            for (int j=0;j<board[i].length;j++) {
+                if (board[1][2] == null) {
+                    return true; //?
                 }
             }
+
         }
 
-        for (Integer[] i: board) {
-            if (!isArrayValid(i)) {
-                System.out.println(6);
-                return false; // row
+        for (int i = 0; i<3;i++) {
+            if (!isArrayValid(board[0])) {
+                return false;
+            }
+            if (!isArrayValid(board[1])) {
+                return false;
+            }
+            if (!isArrayValid(board[2])) {
+                return false; // row ??
             }
         }
 
@@ -59,16 +62,14 @@ public class SudokuSolve {
         Integer [] c2= {board[0][1], board[1][1], board[2][1]};
         Integer [] c3= {board[0][2], board[1][2], board[2][2]};
         Integer [] all = {board[0][0], board[1][0], board[2][0], board[0][1], board[1][1], board[2][1], board[0][2], board[1][2], board[2][2]};
-        if (!isArrayValid(c1) || !isArrayValid(c2) || !isArrayValid(c3) || !isArrayValid(all)) {
+        if (!isArrayValid(c1) || !isArrayValid(c2) || !isArrayValid(c3)) {
             System.out.println(7);
             return false;
         }
-
-
-//        Integer[] column = new Integer[board.length];
-//        for (int i =0;i<board.length;i++) {
-//
-//        }
+        if (!isArrayValid(all)) {
+            System.out.println(7);
+            return false;
+        }
 
         return true;
     }
--- src/W3/test.java
+++ src/W3/test.java
@@ -2,12 +2,7 @@ package W3;
 
 public class test {
     public static void main(String[] args) {
-        for (int i=0; i < 10; i++) {
-            int b = -2 * (i%4 / 3);
-            int a = i%4;
-            double c = (a + b)/2.0 - 0.5;
-
-            System.out.println(c);
-        }
+        Integer a = null;
+        int b = a.intValue();
     }
 }
